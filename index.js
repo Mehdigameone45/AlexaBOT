4c67732a17f4b1802ff31d4527634429dc187fcf
Mehdigameone45 <38880861+Mehdigameone45@users.noreply.github.com>
2018-06-24 20:07:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login('NDYwNDk5NDU0MjUyODEwMjUw.DhFrNA.eebj_YLxVLafibucgYo5EQYC3cA')
+bot.login(process.env.TOKEN)
 
 bot.on("ready", () => { //On dis que quand le bot est prêt, on va faire une action.
     bot.user.setStatus("dnd") //Le bot va se mettre en mode ne pas déranger
